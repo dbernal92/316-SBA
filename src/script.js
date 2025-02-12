224023a5ff142f5af413402cd41ce2ab1afb165b
Darien Bernal <darien.bernal@gmail.com>
2025-02-11 22:53:30 -0500

    Fixed delete buttin functionality

--- src/script.js
+++ src/script.js
@@ -13,19 +13,22 @@ function addNewTask() {
     if (value.length === 0) {
         alert('Please enter a task into the field!');
     } else {
-        const div = document.createElement('div');
-        div.textContent = value;
-        taskList.appendChild(div);
+        const taskItem = document.createElement('li');
+        taskItem.textContent = value;
+        taskList.appendChild(taskItem);
 
         let deleteTaskButton = document.createElement('button');
         deleteTaskButton.setAttribute('id', 'delete-task');
         deleteTaskButton.innerText = 'Remove';
-        div.appendChild(deleteTaskButton);
+        taskItem.appendChild(deleteTaskButton);
+        deleteTaskButton.addEventListener('click', deleteTask);
     }
     taskAddSpace.reset();
 }
 
-
+function deleteTask(event) {
+    event.target.parentElement.remove();
+}
 
 addButton.addEventListener('click', addNewTask);
 
