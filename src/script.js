c9957a3666fc8e526afd667cad5a6396ce051c1b
Darien Bernal <darien.bernal@gmail.com>
2025-02-11 22:14:39 -0500

    Cache elements to values, create function to add tasks to list

--- src/script.js
+++ src/script.js
@@ -1,1 +1,27 @@
-document.getElementById("registration")
+const taskAddSpace = document.getElementById('add-task-here')
+const taskInput = document.getElementById('entered-task');
+const addButton = document.getElementById('add');
+const taskList = document.getElementById('taskList');
+
+console.log(addButton);
+
+let userPendingTasks = [];
+
+function addNewTask() {
+    let value = taskInput.value;
+    
+    if (value.length === 0) {
+        alert('Please enter a task into the field!');
+    } else {
+        const div = document.createElement('div');
+        div.textContent = value;
+        taskList.appendChild(div);
+    }
+    taskAddSpace.reset();
+}
+
+
+
+addButton.addEventListener('click', addNewTask);
+
+console.log(taskList);
