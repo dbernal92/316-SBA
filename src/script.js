015249d162f85ed7fc78f365b9282c89716589c1
Darien Bernal <darien.bernal@gmail.com>
2025-02-11 22:31:17 -0500

    Created delete button for each task added to the list

--- src/script.js
+++ src/script.js
@@ -9,13 +9,18 @@ let userPendingTasks = [];
 
 function addNewTask() {
     let value = taskInput.value;
-    
+
     if (value.length === 0) {
         alert('Please enter a task into the field!');
     } else {
         const div = document.createElement('div');
         div.textContent = value;
         taskList.appendChild(div);
+
+        let deleteTaskButton = document.createElement('button');
+        deleteTaskButton.setAttribute('id', 'delete-task');
+        deleteTaskButton.innerText = 'Remove';
+        div.appendChild(deleteTaskButton);
     }
     taskAddSpace.reset();
 }
